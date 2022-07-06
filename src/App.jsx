c942149db1440fc6a15b99501cd87f2ebf311b72
hamza-che <chenguitiha@gmail.com>
2022-07-06 18:10:35 +0100

    setup development environment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <main>
+      <div className="container">
+        <h1>Hello KF96</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
